faeb5c0af73ca6ba91cba54c0bfa3bcdfc387c29
Mike-415 <mgonz160@mail.ccsf.edu>
2023-02-06 17:45:17 -0800

    Added image, used box to stack text on top of it

--- app/src/main/java/com/example/happybirthdaycard/MainActivity.kt
+++ app/src/main/java/com/example/happybirthdaycard/MainActivity.kt
@@ -3,17 +3,22 @@ package com.example.happybirthdaycard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthdaycard.ui.theme.HappyBirthdayCardTheme
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,10 +27,11 @@ class MainActivity : ComponentActivity() {
             HappyBirthdayCardTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    HappyBirthdayGreeting("You", "me")
+                    BirthdayGreetingWithImage(
+                        getString(R.string.happy_birthday_recipient),
+                        getString(R.string.happy_birthday_sender))
                 }
             }
         }
@@ -33,10 +39,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun HappyBirthdayGreeting(name: String, from: String) {
+fun BirthdayGreetingWithImage(to: String, from: String) {
+    val image = painterResource(R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        BirthdayGreetingWithText(to = to, from = from)
+    }
+}
+@Composable
+fun BirthdayGreetingWithText(to:String, from:String){
     Column {
-        Text(text = "Happy birthday $name!!!!", fontSize = 36.sp)
-        Text(text = "From $from :-)",           fontSize = 24.sp)
+        Text(text = "Happy birthday $to!!!!",
+            fontSize = 36.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth()
+                .padding(start = 16.dp, top = 16.dp))
+        Text(text = "From $from :-)",
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End)
+                .padding(start = 16.dp, end = 16.dp))
     }
 }
 
@@ -44,6 +75,6 @@ fun HappyBirthdayGreeting(name: String, from: String) {
 @Composable
 fun DefaultPreview() {
     HappyBirthdayCardTheme {
-        HappyBirthdayGreeting("You", "Me")
+        BirthdayGreetingWithImage("You", "Me")
     }
 }
